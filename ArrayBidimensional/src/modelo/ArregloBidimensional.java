430c76d716244c3df0ea3d58ed14723a933f685b
Mauro <mauro.kinjuk@gmail.com>
2022-08-28 18:50:31 -0300

    2.E (Array Bidimensional)

---
+++ ArrayBidimensional/src/modelo/ArregloBidimensional.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class ArregloBidimensional {
+	private double[][] matrizA;
+
+	public double[][] getMatrizA() {
+		return matrizA;
+	}
+
+	public void setMatrizA(double[][] matrizA) {
+		this.matrizA = matrizA;
+	}
+
+		this.matrizA = matrizA;
+	}
+}
