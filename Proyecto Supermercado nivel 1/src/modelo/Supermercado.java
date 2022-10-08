1d6ca927d63cd471ae72bcc337672e8114f7d7f0
Mauro <mauro.kinjuk@gmail.com>
2022-10-08 14:31:23 -0300

    Se agrega lo siguiente: Excepciones Traer producto por String Modificar producto Eliminar producto

--- Proyecto Supermercado nivel 1/src/modelo/Supermercado.java
+++ Proyecto Supermercado nivel 1/src/modelo/Supermercado.java
@@ -18,14 +18,21 @@ public class Supermercado {
         this.gondola = gondola;
     }
 
-    public boolean agregarProducto(String producto, float precio){
+    public boolean agregarProducto(String producto, float precio) throws Exception {
         int id = 1;
         if (!gondola.isEmpty()){
             id = gondola.get(gondola.size() - 1).getIdProducto() + 1;
         }
+        /* Si el producto no existe me va a devolver null
+        Por lo tanto compruebo que si al traer el producto es diferente a null
+        Largo una excepcion de que el producto ya existe */
+        if (traerProducto(producto) != null){
+            throw new Exception("Ya existe un producto con ese nombre.");
+        }
         return gondola.add(new Producto(id, producto,precio));
     }
 
+    //Traigo productos por ID y otro por Nombre
     public Producto traerProducto(int idProducto){
         Producto p = null;
         int i = 0;
@@ -40,5 +47,49 @@ public class Supermercado {
         return p;
     }
 
+    public Producto traerProducto(String producto){
+        Producto p = null;
+        int i = 0;
+
+        while(i<gondola.size() && p == null){
+            if (gondola.get(i).getProducto().equals(producto)){
+                p = gondola.get(i);
+            }
+            i++;
+        }
+
+        return p;
+    }
+
+    public boolean modificarProducto(int idProducto, String producto, double precio) throws Exception {
+        boolean flag = false;
+        Producto p = traerProducto(idProducto);
+
+        if(p == null){
+            throw new Exception("El producto no existe.");
+        }else{
+            p.setProducto(producto);
+            /* Como el ejercicio pide modificar con double el precio, pero al mismo tiempo en
+             la declaracion de producto pide que sea float, hago un casteo */
+            p.setPrecio((float) precio);
+            flag = true;
+        }
+
+        return flag;
+    }
+
+    public boolean eliminarProducto(int idProducto) throws Exception {
+        boolean flag = false;
+        Producto p = traerProducto(idProducto);
+
+        if (p == null){
+            throw new Exception("El producto no existe.");
+        }else{
+            gondola.remove(p);
+            flag = true;
+        }
+
+        return flag;
+    }
 
 }
